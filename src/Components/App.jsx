c9925092abe7ff99e20f98c67c9b21870ac02094
tejaspundlik <101443790+tejaspundlik@users.noreply.github.com>
2022-10-26 14:02:30 +0530

    Add props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import Header from "./Header" 
 import Footer from "./Footer"
 import Note from "./Note"
+import notes from "../notes"
 
+function createNotes(item){
+   return (<Note
+    key = {item.key}
+    title={item.title}
+    content={item.content}
+    />);
+}
 function App(){
     return(<div>
         <Header />
-        <Note />
+        {notes.map(createNotes)}
         <Footer />
     </div>);
 }
